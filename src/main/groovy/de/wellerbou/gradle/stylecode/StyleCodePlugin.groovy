ae0e5d1455a1147b4d0644663662db53a753963b
Paul Wellner Bou <paul@wellnerbou.de>
2016-05-02 23:06:47 +0200

    Initial working version

--- src/main/groovy/de/wellerbou/gradle/stylecode/StyleCodePlugin.groovy
+++ src/main/groovy/de/wellerbou/gradle/stylecode/StyleCodePlugin.groovy
@@ -1,5 +1,6 @@
 package de.wellerbou.gradle.stylecode
 
+import de.wellnerbou.stylecode.StyleCodeBuilder
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
@@ -7,11 +8,25 @@ class StyleCodePlugin implements Plugin<Project> {
     void apply(Project project) {
         project.extensions.create("stylecode", StyleCodePluginExtension)
         project.task('styleCode') << {
-            println "Running style-code for ${project.stylecode.markdownfile}..."
+            println "Running style-code for ${project.stylecode.markdownFile}..."
+            new StyleCodeBuilder(project.stylecode.markdownFile)
+                    .useStylesAndScriptsFrom(project.stylecode.urlToFetchResourcesFrom)
+                    .excludeResourcesMatching(project.stylecode.excludePatterns)
+                    .includeInlineScripts(project.stylecode.includeInlineScripts)
+                    .withIframeHtmlTemplate(project.stylecode.iframeTemplate)
+                    .withIndexHtmlTemplate(project.stylecode.indexTemplate)
+                    .build().generate(project.stylecode.outputDirectory);
         }
     }
 }
 
 class StyleCodePluginExtension {
-    String markdownfile
+    String markdownFile;
+    String urlToFetchResourcesFrom;
+    boolean includeInlineScripts = false;
+    Iterable<String> excludePatterns;
+    Iterable<String> additionalResources;
+    String indexTemplate;
+    String iframeTemplate;
+    String outputDirectory;
 }
