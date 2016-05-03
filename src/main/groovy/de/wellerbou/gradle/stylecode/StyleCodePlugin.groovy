c34cab0979a6c8a5dade9e78473d5d6fe0b0e747
Paul Wellner Bou <paul@wellnerbou.de>
2016-05-03 22:46:29 +0200

    Fixes for 0.2

--- src/main/groovy/de/wellerbou/gradle/stylecode/StyleCodePlugin.groovy
+++ src/main/groovy/de/wellerbou/gradle/stylecode/StyleCodePlugin.groovy
@@ -9,20 +9,31 @@ class StyleCodePlugin implements Plugin<Project> {
         project.extensions.create("stylecode", StyleCodePluginExtension)
         project.task('styleCode') << {
             println "Running style-code for ${project.stylecode.markdownFile}..."
-            new StyleCodeBuilder(project.stylecode.markdownFile)
-                    .useStylesAndScriptsFrom(project.stylecode.urlToFetchResourcesFrom)
-                    .excludeResourcesMatching(project.stylecode.excludePatterns)
-                    .includeInlineScripts(project.stylecode.includeInlineScripts)
-                    .withIframeHtmlTemplate(project.stylecode.iframeTemplate)
-                    .withIndexHtmlTemplate(project.stylecode.indexTemplate)
-                    .build().generate(project.stylecode.outputDirectory);
+            StyleCodeBuilder sb = new StyleCodeBuilder(project.stylecode.markdownFile);
+            if(project.stylecode.useResourcesFrom != null) {
+                sb.useStylesAndScriptsFrom(project.stylecode.useResourcesFrom)
+            }
+            if(project.stylecode.excludePatterns != null) {
+                sb.excludeResourcesMatching(project.stylecode.excludePatterns);
+            }
+            if(project.stylecode.additionalResources != null) {
+                sb.withAdditionalResources(project.stylecode.additionalResources);
+            }
+            sb.includeInlineScripts(project.stylecode.includeInlineScripts);
+            if(project.stylecode.iframeTemplate != null) {
+                sb.withIframeHtmlTemplate(project.stylecode.iframeTemplate)
+            }
+            if(project.stylecode.indexTemplate != null) {
+                sb.withIndexHtmlTemplate(project.stylecode.indexTemplate)
+            }
+            sb.build().generate(project.stylecode.outputDirectory == null?project.buildDir.absolutePath+"/styledoc/":project.stylecode.outputDirectory);
         }
     }
 }
 
 class StyleCodePluginExtension {
     String markdownFile;
-    String urlToFetchResourcesFrom;
+    String useResourcesFrom;
     boolean includeInlineScripts = false;
     Iterable<String> excludePatterns;
     Iterable<String> additionalResources;
